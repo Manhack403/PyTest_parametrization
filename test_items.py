ee1fe66f59aa11fa720a4e6aeff42c6a039ac6c9
Manhack403 <137669615+Manhack403@users.noreply.github.com>
2023-07-30 08:30:35 +0300

    Add files via upload

---
+++ test_items.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+class Test_Diff_lang:
+    def test_dif_lang(self, browser):
+
+        browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+        time.sleep(30)
+        search_for_add_to_cart_button = len(browser.find_elements(
+            By.XPATH, "//button[@class='btn btn-lg btn-primary btn-add-to-basket']"))
+        assert search_for_add_to_cart_button > 0, 'There is no button'
